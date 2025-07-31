5ec1b17666748399c40ba23b452c70b7e6f4e977
Muhammad Zeeshan <rafaq657@gmail.com>
2025-07-31 16:51:46 +0500

    Exploring the Structure of SwiftUI App

---
+++ exploring-the-structure-of-a-swiftui-app-sample/AppSample/MyApp.swift
@@ -0,0 +1,43 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+
+/*  import SwiftUI
+ 
+  To Access the symbols and features of SwiftUI, The App uses an import Declaration to import the SwiftUI framework.
+*/
+
+///
+
+/*  @main (attribute)
+    Is Used to idicate the main entry point of the SwiftUI App.
+    
+    The Entry point is responsible for the startup of the app.
+ 
+    A SwiftUI App contains one and only one entry point.
+*/
+
+///
+
+/*  App (protocol)
+    The MyApp Structure conforms to the App protocol, and provides the content of the app and its behavior.
+*/
+
+///
+
+/*  body (property)
+   The Structure implements the computed property body  which is a requirement of the App protocol
+*/
